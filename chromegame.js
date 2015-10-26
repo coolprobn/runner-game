fb4b181e4070330a810b046dcf1d02bd9674bccf
prabin <probnpoudel@gmail.com>
2015-10-26 22:03:46 +0545

    added life of player

--- chromegame.js
+++ chromegame.js
@@ -1,97 +1,109 @@
-	
-	var $gameWindow = document.getElementById('game-window');
-	var $player = document.getElementById('player');
-	$player.style.bottom = 53+ 'px';
-    $player.style.left = 30 + 'px';
-    var posBottom = 0;
-    var count = 0;
-	var jumpUp = function(){
-		posBottom = 150;
-	    $player.style.bottom = posBottom + 'px';
-	};
-	
-	var jumpDown = function(){
-		posBottom = 53;
-        $player.style.bottom = posBottom + 'px';
-	};
+'use strict';
+var $gameWindow = document.getElementById('game-window');
+var $player = document.getElementById('player');
+$player.style.bottom = 53+ 'px';
+$player.style.left = 30 + 'px';
+var posBottom = 0;
+var count = 0;
+var jumpUp = function(){
+    posBottom = 150;
+    $player.style.bottom = posBottom + 'px';
+};
 
-	document.addEventListener('keydown', function(event) {
-    	if(event.keyCode ==38 || event.keyCode == 32){
-    	   jumpUp();
-    	}
-       		
-       	});
-     
-    document.addEventListener('keyup', function(event) {
-    	if(event.keyCode ==38 || event.keyCode == 32){
-	        jumpDown();
-	    };
-    	
-       		
-       	});
+var jumpDown = function(){
+    posBottom = 53;
+    $player.style.bottom = posBottom + 'px';
+};
 
-    var $road = document.getElementById('road');
-    var posRight = -100000;
+document.addEventListener('keydown', function(event) {
+    if(event.keyCode ==38 || event.keyCode == 32){
+        jumpUp();
+    }
 
-    var animateRoad = function(){
-		posRight += 10;
-		$road.style.right = posRight + 'px';
-		$gameWindow.appendChild($road);
-	};
+});
 
-	setInterval(animateRoad, 100);  
+document.addEventListener('keyup', function(event) {
+    if(event.keyCode ==38 || event.keyCode == 32){
+     jumpDown();
+ };
 
-	var $enemy = document.createElement('div');	
-    $enemy.id = 'enemy';
-    $enemy.style.bottom = 53 + 'px';
-    var move = 570;
-    var intervalId;
-    var spawnEnemy = function(){
-    	clearInterval(intervalId);
-        move = 570;
-    	$enemy.style.left = 570 +'px';
-    	$gameWindow.appendChild($enemy);
-    	intervalId = setInterval(moveLeft,100);
-    };
-    var moveLeft = function(){
-        if(move > 0){
+
+});
+
+var $road = document.getElementById('road');
+var posRight = -100000;
+
+var animateRoad = function(){
+    posRight += 10;
+    $road.style.right = posRight + 'px';
+    $gameWindow.appendChild($road);
+};
+
+setInterval(animateRoad, 100);  
+
+var $enemy = document.createElement('div');	
+$enemy.id = 'enemy';
+$enemy.style.bottom = 53 + 'px';
+var move = 570;
+var intervalId;
+var spawnEnemy = function(){
+    clearInterval(intervalId);
+    move = 570;
+    $enemy.style.left = 570 +'px';
+    $gameWindow.appendChild($enemy);
+    intervalId = setInterval(moveLeft,100);
+};
+var moveLeft = function(){
+    if(move > 0){
         move -= 30;
         $enemy.style.left = move + 'px';  
         checkCollision();  
     }else{
         spawnEnemy();
     }
-        
+
+};
+spawnEnemy();
+
+var checkCollision = function(){
+    var enemyBottom = parseInt($enemy.style.bottom);
+    var playerBottom = parseInt($player.style.bottom);
+    var enemyTop = parseInt($enemy.style.bottom) + 50;
+    var playerTop = parseInt($player.style.bottom) + 50;
+    var enemyRight = parseInt(enemy.style.left) + 30;
+    var playerRight = parseInt($player.style.left) + 30;
+    var enemyLeft = parseInt(enemy.style.left);
+    var playerLeft = parseInt($player.style.left);
+	/*if(playerRight>=enemyLeft&&playerRight>enemyLeft&&playerTop<enemyBottom&&playerBottom>=enemyTop){
+		console.log("game over");
+		
+	}*/
+    if(playerRight >= enemyLeft && playerTop == enemyTop && playerBottom == enemyBottom || playerLeft >= enemyLeft && playerBottom <= enemyTop){
+        life();
+        console.log("game over");
+        count = 0;
+    }else{
+        scoreCount();
     };
-   spawnEnemy();
+};
 
-    var checkCollision = function(){
-		var enemyBottom = parseInt($enemy.style.bottom);
-    	var playerBottom = parseInt($player.style.bottom);
-    	var enemyTop = parseInt($enemy.style.bottom) + 50;
-    	var playerTop = parseInt($player.style.bottom) + 50;
-    	var enemyRight = parseInt(enemy.style.left) + 30;
-    	var playerRight = parseInt($player.style.left) + 30;
-    	var enemyLeft = parseInt(enemy.style.left);
-    	var playerLeft = parseInt($player.style.left);
-		/*if(playerRight>=enemyLeft&&playerRight>enemyLeft&&playerTop<enemyBottom&&playerBottom>=enemyTop){
-			console.log("game over");
-			
-		}*/
-        if(playerRight >= enemyLeft && playerTop == enemyTop && playerBottom == enemyBottom || playerLeft >= enemyLeft && playerBottom <= enemyTop){
-            console.log("game over");
-            count = 0;
-        }else{
-            scoreCount();
-        };
-	};
-    
-   var $scoreWindow = document.getElementById('score-window'); 
-   var $score = document.getElementById('score');
-   
-   var scoreCount = function(){
-       count += 10;
-       $score.innerHTML = count;
-       $scoreWindow.appendChild($score);
+var $scoreWindow = document.getElementById('score-window'); 
+var $score = document.getElementById('score');
 
-   };
+var scoreCount = function(){
+   count += 10;
+   $score.innerHTML = count;
+   $scoreWindow.appendChild($score);
+
+};
+
+var $life = document.getElementById('life');
+var lifeCount = 24;
+$life.style.width = 72 + 'px';
+var lifeWidth = parseInt($life.style.width); 
+
+var life = function(){
+   lifeWidth -= lifeCount;
+   $life.style.width = lifeWidth + 'px';
+   $gameWindow.appendChild($life);
+};
